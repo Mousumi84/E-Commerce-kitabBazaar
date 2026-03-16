2182d9d7a2a6ccdfb4c8f545098ce9af06e2c8c3
Mousumi Das <moutikdas8175@gmail.com>
2026-03-16 19:35:49 +0530

    Update - 16/03/2026

---
+++ kb_frontend/src/Components/LoginComponent.js
@@ -0,0 +1,81 @@
+import { Link, useNavigate } from 'react-router-dom';
+import '../Styles/LoginSignup.css';
+import { useContext } from 'react';
+import axios from 'axios';
+import { details } from "../App";
+
+function LoginComponent({ setShowLogin }) {
+    console.log(setShowLogin);
+    const {setIsLogin} = useContext(details);
+    let navigate = useNavigate();
+
+    const loginAPI = async (e) => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        try {
+            const response = await axios({
+                url: `${process.env.REACT_APP_API_URL}/auth/login`,
+                method: "POST",
+                data: {
+                    email:email,
+                    password:password,
+                },
+                withCredentials: true,
+            });
+
+            if(response.data.status !==  200) {
+                alert(response.data.message);
+                return;
+            }
+
+            let token = response.data.token;
+            localStorage.setItem("KitabBaxaarToken",token);
+            localStorage.setItem("User",JSON.stringify(response.data.session.user));
+            setIsLogin(true);
+            setShowLogin(false);
+            console.log(token);
+            // navigate(-1);
+            
+        } catch (error) {
+            console.log(error);
+            alert("An error occured, please try after some time 1234");
+        }
+    }
+
+
+    return  <>
+                <div id="login" className="box-comp" >
+                    <div className="form">
+                        <h3>LOGIN</h3>
+                        <form autoComplete="off" onSubmit={loginAPI}>
+                            <div className="mb">
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">Email</label>
+                                    <input type="email" className="form-control" id="email" />
+                                </div>
+                            </div>
+                            <div className="mb">
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="password" />
+                                </div>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Login</button>
+                        </form>
+                    
+                        <div className='last'>Don't have an account?<Link to={"/signup"}>Signup</Link></div>
+                    </div>
+                </div>
+            </>
+}
+
+export default LoginComponent;
+
+/*
+name Mousumi
+email moutikdas8175@gmail.com
+password: Mousumi@123
+*/
